b2e8558fd6fdb3ad78f57ac891f239741d1b941a
Louis Chatriot <louis.chatriot@gmail.com>
2022-10-23 09:19:41 +0200

    Drawing circles

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
+import matplotlib.pyplot as plt
+
+
+def draw_circle(x, y, r):
+    global plt
+    global axes
+    Drawing_colored_circle = plt.Circle((x, y), r)
+    axes.add_artist(Drawing_colored_circle)
+
+
+figure, axes = plt.subplots()
+draw_circle(.3, .5, .2)
+ 
+axes.set_aspect(1)
+plt.show()
+
+
+
 
 
